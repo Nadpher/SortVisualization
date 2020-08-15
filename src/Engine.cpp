9d85b2adf97c836d2c1067758c6af586df9e57f0
Nadpher <Nadpher@gmail.com>
2020-08-15 23:56:41 +0200

    Working cocktail sort

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -270,6 +270,10 @@ void SortVis::Engine::step()
 		stepSelectionSort();
 		break;
 
+	case SortAlgorithm::cocktailSort:
+		stepCocktailSort();
+		break;
+
 	default:
 		break;
 	}
@@ -308,7 +312,24 @@ void SortVis::Engine::stepSelectionSort()
 
 void SortVis::Engine::stepCocktailSort()
 {
+	static int i = 0;
+	static int size = numbers.size();
+	for (int j = 0; j < size - i - 1; ++j)
+	{
+		if (numbers[j] > numbers[j + 1])
+		{
+			std::swap(numbers[j], numbers[j + 1]);
+		}
+	}
+	++i;
 
+	for (int j = size - i - 1; j > i; --j)
+	{
+		if (numbers[j] < numbers[j - 1])
+		{
+			std::swap(numbers[j], numbers[j - 1]);
+		}
+	}
 }
 
 void SortVis::Engine::draw()
